2a848601de2a8b9652674633553e462412edea58
Raphael Medeiros <medeiros.ph@gmail.com>
2023-03-20 15:57:22 -0300

    functions framework

---
+++ 003-stack_cloud_functions-main/cf_functions_framework/main.py
@@ -0,0 +1,7 @@
+def hello_world(request):
+    request_args = request.args
+    if request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return f'Hello {name}'
