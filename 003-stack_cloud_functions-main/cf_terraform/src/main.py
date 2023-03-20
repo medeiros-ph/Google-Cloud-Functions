1d5295caeb7d2e8294225fd42afacfcb1d6df67e
Raphael Medeiros <medeiros.ph@gmail.com>
2023-03-20 15:56:23 -0300

    terraform

---
+++ 003-stack_cloud_functions-main/cf_terraform/src/main.py
@@ -0,0 +1,9 @@
+def hello_gcs(event, context):
+
+    print('Event ID:' , context.event_id)
+    print('Event type:', context.event_type)
+    print('Bucket:', event['bucket'])
+    print('File:',  event['name'])
+    print('Metageneration:',  event['metageneration'])
+    print('Created:',  event['timeCreated'])
+    print('Updated:',  event['updated'])
